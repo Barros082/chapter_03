0d6b8030e9336fdc7c3897e1b2d4f692fc7721fa
Arthur Barros <arthur.barros.ar@gmail.com>
2026-03-24 08:23:50 -0300

    updating to clone in my second computer

--- distance_urb_agr.R
+++ distance_urb_agr.R
@@ -6,21 +6,11 @@ library(terra)
 library(raster)
 library(sf)
 
-########## preciso ajeitar centroid, a função. 
-#o worflow que pensei é stackar e extrair no mach do
-# nome com uma nova coluna que vou criar usando paste(lu+first_y_t)
-
-########## finishe
 sf_use_s2(FALSE)
-terraOptions(tempdir = here::here("D:/temp"))
+#terraOptions(tempdir = here::here("D:/temp"))
 #terraOptions()$tempdir
 
 PA<-read_sf("Outputs/PA_br.gpkg")
-
-centroid_PA5880<-PA %>% 
-  st_transform(., "EPSG:5880") %>% 
-  st_centroid() %>% 
-  dplyr::select(new_code)
 
 LU<-list.files("DATA/Mapbiomas/LU", 
                full.names = TRUE)
@@ -29,7 +19,15 @@ LU<-list.files("DATA/Mapbiomas/LU",
 n_LU<-c("LU_1990", "LU_1995", "LU_2000", 
         "LU_2005", "LU_2010", "LU_2015")
 
-agg_dist_extract<-function(x){
+centroid_PA5880<-PA %>% 
+  st_transform(., "EPSG:5880") %>% 
+  st_centroid() %>% 
+  dplyr::select(new_code, first_year_t) %>% 
+  group_by(new_code) %>% 
+  group_split()
+
+
+agg_dist_extract<-function(x, centr_i){
   x_agg <- aggregate(
     x,
     fact = 2,   #~60 m pixels
@@ -40,10 +38,10 @@ agg_dist_extract<-function(x){
   
   extracting_x <- terra::extract(
     dist_x,
-    vect(centroid_PA5880),
+    vect(centr_i),
     bind = TRUE)
-  return(extracting_x)
   beepr::beep()
+  return(extracting_x)
 }
 
 
@@ -52,11 +50,41 @@ agg_dist_extract<-function(x){
 urban_px<-list()
 for (i in seq_along(LU)) {
   r_i<-rast(LU[[i]])
+  nm_lu<-n_LU[i]
   r_urb <- ifel(r_i==24, 1, NA)
-  r_urb_proj <- project(r_urb, "EPSG:5880") 
-  urban_px[[n_LU]] <- r_urb_proj
+  r_urb_proj <- project(
+    r_urb, "EPSG:5880",
+    filename = paste0("Outputs/temp_proj_", nm_lu, ".tif"),
+    overwrite = TRUE,
+    wopt = list(gdal = c("COMPRESS=LZW"))) 
+  
+  urban_px[[nm_lu]] <- r_urb_proj
 }
 beepr::beep()
+
+dist_urb<-list()
+for (i in seq_along(centroid_PA5880)) {
+  centr_i<-centroid_PA5880[[i]]
+  
+  if(centr_i$first_year_t%in%c(1991, 0)){
+    K=urban_px[["LU_1990"]]}
+  if(centr_i$first_year_t==1996){
+    K=urban_px[["LU_1995"]]}
+  if(centr_i$first_year_t==2001){
+    K=urban_px[["LU_2000"]]}
+  if(centr_i$first_year_t==2006){
+    K=urban_px[["LU_2005"]]}
+  if(centr_i$first_year_t==2011){
+    K=urban_px[["LU_2010"]]}
+  if(centr_i$first_year_t==2016){
+    K=urban_px[["LU_2015"]]}
+  
+  dist_urb[[i]]<-agg_dist_extract(K, centr_i)
+}
+dist_urb_px<-do.call(rbind, dist_urb)
+
+
+
 
 # Agriculture ----
 
